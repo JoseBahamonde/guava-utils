525c6b7c01e3b4301e042df2366a6dc6cc47f041
José Bahamonde Cordeiro <jbahamondecordeiro@gmail.com>
2018-07-28 17:07:10 +0200

    Se añade utilidad para poder obtener siempre el mismo objeto a partir de un valor primitivo de modo que se puedan usar en un bloque syncronized

---
+++ src/main/java/com/bahamonde/utils/locks/PrimitiveLock.java
@@ -0,0 +1,36 @@
+package com.bahamonde.utils.locks;
+
+/**
+ * Utility to ease de synchronization with primitive values. Returns the same
+ * object for de same primitive value.
+ * 
+ * @author jbahamonde
+ *
+ */
+public class PrimitiveLock {
+
+	public Object of(int value) {
+		return String.valueOf(value).intern();
+	}
+
+	public Object of(long value) {
+		return String.valueOf(value).intern();
+	}
+
+	public Object of(double value) {
+		return String.valueOf(value).intern();
+	}
+
+	public Object of(float value) {
+		return String.valueOf(value).intern();
+	}
+
+	public Object of(byte value) {
+		return String.valueOf(value).intern();
+	}
+
+	public Object of(char value) {
+		return String.valueOf(value).intern();
+	}
+
+}
