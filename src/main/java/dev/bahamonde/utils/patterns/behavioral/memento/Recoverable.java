aa60633118499b6ed284e25ca09d30c7968421ea
bahamondev <jose@bahamonde.dev>
2020-04-13 01:31:13 +0200

    docs: Purpose now documented

--- src/main/java/dev/bahamonde/utils/patterns/behavioral/memento/Recoverable.java
+++ src/main/java/dev/bahamonde/utils/patterns/behavioral/memento/Recoverable.java
@@ -1,7 +1,7 @@
 package main.java.dev.bahamonde.utils.patterns.behavioral.memento;
 
 /**
- * Recoverable object with memento pattern
+ * Recoverable object with memento pattern. Types that could be recovered should implement this
  * @param <M> The memento type for this object
  */
 public interface Recoverable<M extends Memento> {
