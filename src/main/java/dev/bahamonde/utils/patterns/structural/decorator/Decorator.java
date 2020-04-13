fd4cb4f8dd0cb9e54d9818a688a865743602152b
bahamondev <jose@bahamonde.dev>
2020-04-13 21:50:29 +0200

    feat: Added decorator pattern

---
+++ src/main/java/dev/bahamonde/utils/patterns/structural/decorator/Decorator.java
@@ -0,0 +1,20 @@
+package main.java.dev.bahamonde.utils.patterns.structural.decorator;
+
+/**
+ * Definition for the decorator pattern. It wraps the decorated components. Its intention is also provides a base
+ * documentation for the concrete decorator.
+ * @param <T> The type to decorate
+ */
+public class Decorator<T> {
+
+    private final T component;
+
+    public Decorator(T component) {
+        this.component = component;
+    }
+
+    protected T getComponent() {
+        return this.component;
+    }
+
+}
